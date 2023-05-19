bcdfb177d127b52d1da908c2bd19b36e15eb7716
krzylesniewski <krzysztof.lesniewski.wroc@gmail.com>
2023-05-20 00:59:04 +0200

    player basic animation

--- Player/Player.cs
+++ Player/Player.cs
@@ -7,7 +7,8 @@ public partial class Player : CharacterBody2D
 	public const float JumpVelocity = -400.0f;
 
 	public AnimationPlayer _animationPlayer;
-	public Sprite2D _sprite2dRun;
+	public Sprite2D _sprite2dIdle;
+	public Sprite2D _sprite2dWalk;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
@@ -15,10 +16,13 @@ public partial class Player : CharacterBody2D
 	public override void _Ready()
 	{
 		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-		_sprite2dRun = GetNode<Sprite2D>($"Sprite2D_run");
+		_sprite2dIdle = GetNode<Sprite2D>($"Sprite2D_idle");
+		_sprite2dWalk = GetNode<Sprite2D>($"Sprite2D_walk");
 
 
 		_animationPlayer.Play("idle_front");
+		_sprite2dIdle.Visible = true;
+		_sprite2dWalk.Visible = false;
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -30,13 +34,15 @@ public partial class Player : CharacterBody2D
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down").Normalized();
 		if (direction != Vector2.Zero)
 		{
-			_animationPlayer.Play("run_side");
+			_animationPlayer.Play("walk_side");
+			_sprite2dIdle.Visible = false;
+			_sprite2dWalk.Visible = true;
 			if(direction.X > 0)
 			{
-				_sprite2dRun.FlipH = true;
+				_sprite2dWalk.FlipH = true;
 			} else
 			{
-				_sprite2dRun.FlipH = false;
+				_sprite2dWalk.FlipH = false;
 			}
 			
 			velocity.X = direction.X * Speed;
@@ -44,7 +50,7 @@ public partial class Player : CharacterBody2D
 		}
 		else
 		{
-			_animationPlayer.Play("idle_front");
+			// _animationPlayer.Play("idle_front");
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
@@ -52,6 +58,8 @@ public partial class Player : CharacterBody2D
 		if(direction == Vector2.Zero)
 		{
 			_animationPlayer.Play("idle_front");
+			_sprite2dIdle.Visible = true;
+			_sprite2dWalk.Visible = false;
 		}
 
 		Velocity = velocity;
